c300ad4ed9b54ae40092b4e47eca26c6fa10c603
Wu Yao <SENSETIME\wuyao@g105e1800191.domain.sensetime.com>
2019-01-29 19:06:55 +0800

    add v23_aug and vnetprun2

--- modeling/__init__.py
+++ modeling/__init__.py
@@ -3,5 +3,9 @@ from .msc import MSC
 from .v23 import V23_4x
 from .vnet3_360 import Vnet3_360
 from .vnet_pruning_1 import VnetPrun1
+from .vnet_pruning_2 import VnetPrun2
+from .v23_aug import V23_aug
 
 network_map = {'v23_4x':V23_4x,'vnet3_360':Vnet3_360,'dbl':Dbl,'msc':MSC,'vnetprun1':VnetPrun1}
+network_map['v23aug'] = V23_aug
+network_map['vnetprun2'] = VnetPrun2
